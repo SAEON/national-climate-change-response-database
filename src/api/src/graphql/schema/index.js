2e56a4763bbdf7ef8afaf67326b7c2192b23b0a0
Zach Smith <zachsmith.sa@gmail.com>
2021-05-07 13:15:51 +0200

    Updated graphql schema layout to be easier to read

--- src/api/src/graphql/schema/index.js
+++ src/api/src/graphql/schema/index.js
@@ -1,15 +1,19 @@
 import { makeExecutableSchema } from 'graphql-tools'
 import { join } from 'path'
-import { readFileSync } from 'fs'
+import { readFileSync, readdirSync } from 'fs'
 import getCurrentDirectory from '../../lib/get-current-directory.js'
 import * as resolvers from '../resolvers/index.js'
+
+const __dirname = getCurrentDirectory(import.meta)
+
+const SCHEMA_PARTS = readdirSync(join(__dirname, './type-defs'))
 
 const _import = p =>
   readFileSync(join(getCurrentDirectory(import.meta), p), {
     encoding: 'utf-8',
   })
 
-const typeDefs = `${_import('./_schema.graphql')}`
+const typeDefs = SCHEMA_PARTS.map(name => `${_import(`./type-defs/${name}`)}`).join('\n')
 
 const schema = makeExecutableSchema({
   typeDefs,

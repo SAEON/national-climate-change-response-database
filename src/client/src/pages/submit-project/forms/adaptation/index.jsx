61f7da2d350c9d127ed5e7362a0fc9e02329ef29
Zach Smith <zachsmith.sa@gmail.com>
2021-06-17 05:56:08 +0200

    Improved rendering performance of mitigation and adaptations forms ui (not specific fields yet)

--- src/client/src/pages/submit-project/forms/adaptation/index.jsx
+++ src/client/src/pages/submit-project/forms/adaptation/index.jsx
@@ -1,5 +1,5 @@
 import { useContext, memo } from 'react'
-import { context as formContext } from '../../gql-form-binder'
+import { context as formContext, ComposeForm } from '../../gql-form-binder'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import PlusIcon from 'mdi-react/PlusIcon'
@@ -8,90 +8,121 @@ import useTheme from '@material-ui/core/styles/useTheme'
 import FormIcon from 'mdi-react/PencilIcon'
 import DeleteIcon from 'mdi-react/DeleteIcon'
 import RenderField from './_render-field'
-import { ComposeForm } from '../../gql-form-binder'
 
-const Compose = memo(({ fields }) => {
-  const theme = useTheme()
-  const { adaptationForms, addAdaptationForm, removeAdaptationForm } = useContext(formContext)
+const Compose = memo(
+  ({ i, form, fields, removeForm }) => {
+    const theme = useTheme()
 
-  return (
-    <>
-      {adaptationForms.map((form, i) => {
-        return (
-          <div key={i} style={{ marginBottom: theme.spacing(2) }}>
-            <Collapse
-              avatarStyle={{ backgroundColor: theme.palette.primary.light }}
-              Icon={FormIcon}
-              title={form.title || 'No title'}
-              actions={[
-                <IconButton
-                  onClick={e => {
-                    e.stopPropagation()
-                    removeAdaptationForm(i)
-                  }}
-                  key="delete"
-                >
-                  <DeleteIcon />
-                </IconButton>,
-              ]}
+    return (
+      <div key={i} style={{ marginBottom: theme.spacing(2) }}>
+        <Collapse
+          avatarStyle={{ backgroundColor: theme.palette.primary.light }}
+          Icon={FormIcon}
+          title={form.title || 'No title'}
+          actions={[
+            <IconButton
+              onClick={e => {
+                e.stopPropagation()
+                removeForm(i)
+              }}
+              key="delete"
             >
-              <div style={{ padding: theme.spacing(2) }}>
-                <ComposeForm
-                  RenderField={RenderField}
-                  fields={fields}
-                  formNumber={i}
-                  cardStyle={{ backgroundColor: 'transparent', border: 'none' }}
-                  cardContentStyle={{ paddingLeft: 0, paddingRight: 0 }}
-                  cardHeaderStyle={{ paddingLeft: 0, paddingRight: 0 }}
-                  sections={{
-                    'Adaptation details': [
-                      'title',
-                      'description',
-                      'adaptationPurpose',
-                      'adaptationSector',
-                      'interventionStatus',
-                      'startYear',
-                      'endYear',
-                    ],
-                    'Location(s)': ['yx'],
-                    'Associated research': [
-                      'isResearch',
-                      'researchDescription',
-                      'researchType',
-                      'researchTargetAudience',
-                      'researchAuthor',
-                      'researchPaper',
-                    ],
-                    'Hazard details': ['hazardFamily', 'hazardSubFamily', 'hazard', 'subHazard'],
-                  }}
-                />
-              </div>
-            </Collapse>
-          </div>
-        )
-      })}
-      <div style={{ display: 'flex' }}>
-        <Button
-          disableElevation
-          variant="contained"
-          onClick={addAdaptationForm}
-          endIcon={<PlusIcon />}
-          size="large"
-          color="default"
-          style={{ marginRight: 'auto' }}
+              <DeleteIcon />
+            </IconButton>,
+          ]}
         >
-          Add adaptation component
+          <div style={{ padding: theme.spacing(2) }}>
+            <ComposeForm
+              RenderField={RenderField}
+              fields={fields}
+              formNumber={i}
+              cardStyle={{ backgroundColor: 'transparent', border: 'none' }}
+              cardContentStyle={{ paddingLeft: 0, paddingRight: 0 }}
+              cardHeaderStyle={{ paddingLeft: 0, paddingRight: 0 }}
+              sections={{
+                'Adaptation details': [
+                  'title',
+                  'description',
+                  'adaptationPurpose',
+                  'adaptationSector',
+                  'interventionStatus',
+                  'startYear',
+                  'endYear',
+                ],
+                'Location(s)': ['yx'],
+                'Associated research': [
+                  'isResearch',
+                  'researchDescription',
+                  'researchType',
+                  'researchTargetAudience',
+                  'researchAuthor',
+                  'researchPaper',
+                ],
+                'Hazard details': ['hazardFamily', 'hazardSubFamily', 'hazard', 'subHazard'],
+              }}
+            />
+          </div>
+        </Collapse>
+      </div>
+    )
+  },
+  ({ i: iA, form: { ...fieldsA } }, { i: iB, form: { ...fieldsB } }) => {
+    let _memo = true
+    if (iA !== iB) _memo = false
+    if (fieldsA.title !== fieldsB.title) _memo = false
+    return _memo
+  }
+)
+
+const Form = ({ i, fields, removeForm }) => {
+  const { adaptationForms: forms } = useContext(formContext)
+  return <Compose i={i} form={forms[i]} fields={fields} removeForm={removeForm} />
+}
+
+/**
+ * This component should only render if the
+ * number of forms changes
+ */
+const RenderForms = memo(
+  ({ fields, forms, addForm, removeForm }) => (
+    <>
+      {forms.map((form, i) => (
+        <Form key={i} i={i} fields={fields} removeForm={removeForm} />
+      ))}
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Button
+          variant="contained"
+          onClick={addForm}
+          startIcon={<PlusIcon />}
+          size="large"
+          color="inherit"
+        >
+          Add details
         </Button>
       </div>
     </>
-  )
-})
+  ),
+  ({ forms: a }, { forms: b }) => {
+    let _memo = true
+    if (a.length !== b.length) _memo = false
+    return _memo
+  }
+)
 
 /**
- * Don't render ComposeForm directly,
- * as that will trigger many re-renders
+ * This component should only render if the
+ * number of forms changes
  */
 export default () => {
-  const { adaptationFields } = useContext(formContext)
-  return <Compose fields={adaptationFields} />
+  const { adaptationFields, adaptationForms, addAdaptationForm, removeAdaptationForm } =
+    useContext(formContext)
+
+  return (
+    <RenderForms
+      fields={adaptationFields}
+      forms={adaptationForms}
+      addForm={addAdaptationForm}
+      removeForm={removeAdaptationForm}
+    />
+  )
 }
--- src/client/src/pages/submit-project/forms/mitigation/index.jsx
+++ src/client/src/pages/submit-project/forms/mitigation/index.jsx
@@ -1,115 +1,141 @@
-import { useContext } from 'react'
+import { useContext, memo } from 'react'
 import { context as formContext, ComposeForm } from '../../gql-form-binder'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
-import PlusIcon from 'mdi-react/PlusIcon'
+import PlusIcon from 'mdi-react/PlusCircleIcon'
 import Collapse from '../../../../components/collapse'
 import useTheme from '@material-ui/core/styles/useTheme'
 import FormIcon from 'mdi-react/PencilIcon'
 import DeleteIcon from 'mdi-react/DeleteIcon'
 import RenderField from './_render-field'
 
-const Compose = ({ fields }) => {
-  const theme = useTheme()
-  const { mitigationForms, addMitigationForm, removeMitigationForm } = useContext(formContext)
+const Compose = memo(
+  ({ form, i, removeForm, fields }) => {
+    const theme = useTheme()
 
-  return (
-    <>
-      {mitigationForms.map((form, i) => {
-        return (
-          <div key={i} style={{ marginBottom: theme.spacing(2) }}>
-            <Collapse
-              avatarStyle={{ backgroundColor: theme.palette.primary.light }}
-              Icon={FormIcon}
-              title={form.title || 'No title'}
-              actions={[
-                <IconButton
-                  onClick={e => {
-                    e.stopPropagation()
-                    removeMitigationForm(i)
-                  }}
-                  key="delete"
-                >
-                  <DeleteIcon />
-                </IconButton>,
-              ]}
+    return (
+      <div style={{ marginBottom: theme.spacing(2) }}>
+        <Collapse
+          avatarStyle={{ backgroundColor: theme.palette.primary.light }}
+          Icon={FormIcon}
+          title={form.title || 'No title'}
+          actions={[
+            <IconButton
+              onClick={e => {
+                e.stopPropagation()
+                removeForm(i)
+              }}
+              key="delete"
             >
-              <div style={{ padding: theme.spacing(2) }}>
-                <ComposeForm
-                  RenderField={RenderField}
-                  fields={fields}
-                  formNumber={i}
-                  cardStyle={{ backgroundColor: 'transparent', border: 'none' }}
-                  cardContentStyle={{ paddingLeft: 0, paddingRight: 0 }}
-                  cardHeaderStyle={{ paddingLeft: 0, paddingRight: 0 }}
-                  sections={{
-                    'Mitigation information': [
-                      'title',
-                      'description',
-                      'mitigationType',
-                      'mitigationSubType',
-                      'interventionStatus',
-                    ],
-                    'Location(s)': ['yx'],
-
-                    'Associated research': [
-                      'isResearch',
-                      'researchDescription',
-                      'researchType',
-                      'researchTargetAudience',
-                      'researchAuthor',
-                      'researchPaper',
-                    ],
-                    'Energy/emissions data': [
-                      'energyOrEmissionsData',
-                      'energyData',
-                      'emissionsData',
-                    ],
-                    'Carbon credit program': [
-                      'carbonCredit',
-                      'volMethodology',
-                      'goldStandard',
-                      'vcs',
-                      'otherCarbonCreditStandard',
-                      'otherCarbonCreditStandardDescription',
-                    ],
-                    'CDM information': [
-                      'cdmProjectNumber',
-                      'cdmMethodology',
-                      'cdmExecutiveStatus',
-                      'cdmStatus',
-                    ],
-                    'IPCC information': [
-                      'hostSector',
-                      'hostSubSectorPrimary',
-                      'hostSubSectorSecondary',
-                    ],
-                  }}
-                />
-              </div>
-            </Collapse>
-          </div>
-        )
-      })}
-      <div style={{ display: 'flex' }}>
-        <Button
-          disableElevation
-          variant="contained"
-          onClick={addMitigationForm}
-          endIcon={<PlusIcon />}
-          size="large"
-          color="default"
-          style={{ marginRight: 'auto' }}
+              <DeleteIcon />
+            </IconButton>,
+          ]}
         >
-          Add mitigation component
+          <div style={{ padding: theme.spacing(2) }}>
+            <ComposeForm
+              RenderField={RenderField}
+              fields={fields}
+              formNumber={i}
+              cardStyle={{ backgroundColor: 'transparent', border: 'none' }}
+              cardContentStyle={{ paddingLeft: 0, paddingRight: 0 }}
+              cardHeaderStyle={{ paddingLeft: 0, paddingRight: 0 }}
+              sections={{
+                'Mitigation information': [
+                  'title',
+                  'description',
+                  'mitigationType',
+                  'mitigationSubType',
+                  'interventionStatus',
+                ],
+                'Location(s)': ['yx'],
+                'Associated research': [
+                  'isResearch',
+                  'researchDescription',
+                  'researchType',
+                  'researchTargetAudience',
+                  'researchAuthor',
+                  'researchPaper',
+                ],
+                'Energy/emissions data': ['energyOrEmissionsData', 'energyData', 'emissionsData'],
+                'Carbon credit program': [
+                  'carbonCredit',
+                  'volMethodology',
+                  'goldStandard',
+                  'vcs',
+                  'otherCarbonCreditStandard',
+                  'otherCarbonCreditStandardDescription',
+                ],
+                'CDM information': [
+                  'cdmProjectNumber',
+                  'cdmMethodology',
+                  'cdmExecutiveStatus',
+                  'cdmStatus',
+                ],
+                'IPCC information': [
+                  'hostSector',
+                  'hostSubSectorPrimary',
+                  'hostSubSectorSecondary',
+                ],
+              }}
+            />
+          </div>
+        </Collapse>
+      </div>
+    )
+  },
+  ({ i: iA, form: { ...fieldsA } }, { i: iB, form: { ...fieldsB } }) => {
+    let _memo = true
+    if (iA !== iB) _memo = false
+    if (fieldsA.title !== fieldsB.title) _memo = false
+    return _memo
+  }
+)
+
+const Form = ({ i, fields, removeForm }) => {
+  const { mitigationForms: forms } = useContext(formContext)
+  return <Compose i={i} form={forms[i]} fields={fields} removeForm={removeForm} />
+}
+
+/**
+ * This component should only render if the
+ * number of forms changes
+ */
+const RenderForms = memo(
+  ({ fields, forms, addForm, removeForm }) => (
+    <>
+      {forms.map((form, i) => (
+        <Form key={i} i={i} fields={fields} removeForm={removeForm} />
+      ))}
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Button
+          variant="contained"
+          onClick={addForm}
+          startIcon={<PlusIcon />}
+          size="large"
+          color="inherit"
+        >
+          Add details
         </Button>
       </div>
     </>
-  )
-}
+  ),
+  ({ forms: a }, { forms: b }) => {
+    let _memo = true
+    if (a.length !== b.length) _memo = false
+    return _memo
+  }
+)
 
 export default () => {
-  const { mitigationFields } = useContext(formContext)
+  const { mitigationFields, mitigationForms, addMitigationForm, removeMitigationForm } =
+    useContext(formContext)
 
-  return <Compose fields={mitigationFields} />
+  return (
+    <RenderForms
+      fields={mitigationFields}
+      forms={mitigationForms}
+      addForm={addMitigationForm}
+      removeForm={removeMitigationForm}
+    />
+  )
 }

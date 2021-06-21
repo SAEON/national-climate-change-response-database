c7b69e4c0d4c1f3078b4f40a546d9213f3e86d38
Zach Smith <zachsmith.sa@gmail.com>
2021-06-21 15:46:59 +0200

    Limited lines to 3

--- src/client/src/pages/projects/results/result-card/_description.jsx
+++ src/client/src/pages/projects/results/result-card/_description.jsx
@@ -1,10 +1,29 @@
 import Typography from '@material-ui/core/Typography'
 import CardContent from '@material-ui/core/CardContent'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+import clsx from 'clsx'
+
+const useStyles = makeStyles(() => ({
+  text: {
+    fontSize: '0.8rem',
+    lineHeight: 1.5,
+    whiteSpace: 'break-spaces',
+    wordBreak: 'break-word',
+    lineClamp: 3,
+    boxOrient: 'vertical',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    display: '-webkit-box',
+  },
+}))
 
 export default ({ description }) => {
+  const classes = useStyles()
   return (
     <CardContent>
-      <Typography variant="body2">{description || 'No description'}</Typography>
+      <Typography className={clsx(classes.text)} variant="body2">
+        {description || 'No description'}
+      </Typography>
     </CardContent>
   )
 }

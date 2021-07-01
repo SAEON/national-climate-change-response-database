f47e09952b8792f293c7f13a14ea6a481abed477
Zach Smith <zachsmith.sa@gmail.com>
2021-07-01 08:56:23 +0200

    fixed login for new submission

--- src/client/src/pages/create-submission/index.jsx
+++ src/client/src/pages/create-submission/index.jsx
@@ -1,9 +1,16 @@
+import { useContext } from 'react'
 import { useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 import { gql, useMutation } from '@apollo/client'
 import Loading from '../../components/loading'
+import { context as authenticationContext } from '../../contexts/authentication'
+import { context as authorizationContext } from '../../contexts/authorization'
+import AccessDenied from '../../components/access-denied'
 
 export default () => {
+  const isAuthenticated = useContext(authenticationContext).authenticate()
+  const { hasPermission } = useContext(authorizationContext)
+
   const [createSubmission, { data: { createSubmission: id } = {} }] = useMutation(
     gql`
       mutation createSubmission {
@@ -15,6 +22,14 @@ export default () => {
   useEffect(() => {
     createSubmission()
   }, [createSubmission])
+
+  if (!isAuthenticated) {
+    return <Loading />
+  }
+
+  if (!hasPermission('create-project')) {
+    return <AccessDenied requiredPermission="create-project" />
+  }
 
   if (id) {
     return <Redirect to={`/projects/submission/${id}`} />

4852ebde9895928be82727a8637a3a3eda415388
Zach Smith <zachsmith.sa@gmail.com>
2021-07-02 11:22:30 +0200

    Implemented the share link

--- src/client/src/pages/submit-project/header/share-form/index.jsx
+++ src/client/src/pages/submit-project/header/share-form/index.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import Icon from 'mdi-react/ContentCopyIcon'
 import Button from '@material-ui/core/Button'
 import Hidden from '@material-ui/core/Hidden'
 import MessageDialogue from '../../../../components/message-dialogue'
+import useTheme from '@material-ui/core/styles/useTheme'
+import Typography from '@material-ui/core/Typography'
+import CheckIcon from 'mdi-react/CheckCircleIcon'
 
 export default () => {
+  const [copied, setCopied] = useState(false)
+  const theme = useTheme()
+
   return (
     <MessageDialogue
-      title="Share submission form"
-      text={'Use this link to come back to the form later, and continue editing it'}
+      title="Share submission form link"
+      text={
+        <>
+          <Typography gutterBottom>
+            This form will be available on the link below until it is submitted. After the form is
+            submitted you can search for this submission on the project page and edit from there
+          </Typography>
+          <pre style={{ ...theme.pre }}>{window.location.href}</pre>
+        </>
+      }
       tooltipProps={{
         placement: 'bottom',
         title: 'Share form',
@@ -40,16 +55,20 @@ export default () => {
         closeFn => (
           <Button
             key="delete-project"
-            startIcon={<Icon size={18} />}
+            startIcon={copied ? <CheckIcon size={18} /> : <Icon size={18} />}
             onClick={e => {
-              closeFn(e)
+              navigator.clipboard.writeText(window.location.href)
+              setCopied(true)
+              if (copied) {
+                closeFn(e)
+              }
             }}
             color="primary"
             size="small"
             variant="contained"
             disableElevation
           >
-            Copy link (not implemented yet)
+            {copied ? 'Copied' : 'Copy link'}
           </Button>
         ),
       ]}

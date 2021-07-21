8dcdb75dd9c2c19fd796063b213220f772a56750
Zach Smith <zachsmith.sa@gmail.com>
2021-07-21 10:15:21 +0200

    Updated ERM integration SQL, only GPS coordinates outstanding

--- src/api/src/graphql/resolvers/mutations/seed-database/erm/erm.sql
+++ src/api/src/graphql/resolvers/mutations/seed-database/erm/erm.sql
@@ -2,8 +2,28 @@ use NCCRD_ERM;
 go
 
 ;with projectDetails as (
-  select
+  select distinct
     p.ProjectDetailsId,
+    (select ProvinceName province
+     from tb_erm_Province
+     join tb_erm_Project_Location_Data ld on ld.ProjectDetailsId = p.ProjectDetailsId
+     where ProvinceID = ld.Province for json path
+    ) province,
+    (select MetroName districtMunicipality
+     from tb_erm_Metro_DistrictMunicipality
+     join tb_erm_Project_Location_Data ld on ld.ProjectDetailsId = p.ProjectDetailsId
+     where MetroID = ld.Metro for json path
+    ) districtMunicipality,
+    (select LocalMunicipalityName localMunicipality
+     from tb_erm_Local_Municipalities
+     join tb_erm_Project_Location_Data ld on ld.ProjectDetailsId = p.ProjectDetailsId
+     where LocalMunicipalityID = ld.LocalMunicipality for json path
+    ) localMunicipality,
+    (select TownName cityOrTown
+     from tb_erm_Town
+     join tb_erm_Project_Location_Data ld on ld.ProjectDetailsId = p.ProjectDetailsId
+     where TownID = ld.Town for json path
+    ) cityOrTown,
     p.ProjectTitle title,
     p.Description description,
     case TypeOfIntervention
@@ -20,11 +40,46 @@ go
     replace(CONCAT('R', coalesce(ltrim(str(p.BudgetLower, 25,0)), '0'), ' - R', coalesce(ltrim(str(p.BudgetUpper, 25, 0)), '0')), 'R0 - R0', '') estimatedBudget
 
   from tb_erm_project_details p
+  left join tb_erm_Project_Location_Data ld on ld.ProjectDetailsId = p.ProjectDetailsId
 )
+
 
 ,mitigationDetails as (
   select
     ProjectDetailsId,
+    (select
+      e.Year,
+      ltrim(str(e.CO2, 25, 0)) CO2,
+      ltrim(str(e.CH4, 25, 0)) CH4,
+      ltrim(str(e.CH4_CO2e, 25, 0)) CH4_CO2e,
+      ltrim(str(e.N2O, 25, 0)) N2O,
+      ltrim(str(e.N2O_CO2e, 25, 0)) N2O_CO2e,
+      ltrim(str(e.HFC, 25, 0)) HFC,
+      ltrim(str(e.HFC_CO2e, 25, 0)) HFC_CO2e,
+      ltrim(str(e.PFC, 25, 0)) PFC,
+      ltrim(str(e.PFC_CO2e, 25, 0)) PFC_CO2e,
+      ltrim(str(e.SF6, 25, 0)) SF6,
+      ltrim(str(e.SF6_CO2e, 25, 0)) SF6_CO2e,
+      ltrim(str(e.Hydro, 25, 0)) Hydro,
+      ltrim(str(e.Tidal, 25, 0)) Tidal,
+      ltrim(str(e.Wind, 25, 0)) Wind,
+      ltrim(str(e.Solar, 25, 0)) Solar,
+      ltrim(str(e.FossilFuelElecRed, 25, 0)) FossilFuelElecRed,
+      ltrim(str(e.BioWaste, 25, 0)) BioWaste,
+      ltrim(str(e.BioWaste_CO2e, 25, 0)) BioWaste_CO2e,
+      ltrim(str(e.Geothermal, 25, 0)) Geothermal,
+      ltrim(str(e.Geothermal_CO2e, 25, 0)) Geothermal_CO2e,
+      ltrim(str(e.Hydro_CO2e, 25, 0)) Hydro_CO2e,
+      ltrim(str(e.Solar_CO2e, 25, 0)) Solar_CO2e,
+      ltrim(str(e.Tidal_CO2e, 25, 0)) Tidal_CO2e,
+      ltrim(str(e.Wind_CO2e, 25, 0)) Wind_CO2e,
+      ltrim(str(e.FossilFuelElecRed_CO2e, 25, 0)) FossilFuelElecRed_CO2e
+     from tb_erm_mitigation_details _m
+     join tb_erm_Project_Location_Data ld on ld.ProjectDetailsId = _m.ProjectDetailsId
+     join tb_erm_Mitigation_Emissions_Data e on e.ProjectLocationDataId = ld.ProjectLocationDataId
+     where _m.ProjectDetailsId = m.ProjectDetailsId
+     for json path
+    ) _ermDbEmissions,
     (select ItemDisplay from tb_erm_picklist l join tb_erm_picklist_value v on v.ListId = l.ListId where l.ListName = 'Type of Project' and ItemNum = m.ProjectType) mitigationType,
     (select ProjectSubType from tb_erm_Project_SubType where ProjectSubTypeId = m.ProjectSubType) mitigationSubType,
     (select ItemDisplay from tb_erm_picklist l join tb_erm_picklist_value v on v.ListId = l.ListId where l.ListName = 'Host sector' and ItemNum = m.HostSector) hostSector,
@@ -95,7 +150,4 @@ select
 *
 from submissions
 
-TODO
-
-(1) location data for project
-(2) emission data for mitigation
+(1) GPS coordinates

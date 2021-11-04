f2ef20e7b629dcfdc7b5fc479ebd4ce208437618
Zach Smith <zachsmith.sa@gmail.com>
2021-11-04 11:31:25 +0200

    Fixed sql error

--- src/api/src/mssql/data-loaders/_search-vocabulary-tree.js
+++ src/api/src/mssql/data-loaders/_search-vocabulary-tree.js
@@ -1,18 +1,19 @@
 import DataLoader from 'dataloader'
 import { pool } from '../pool.js'
 import sift from 'sift'
+import logSql from '../../lib/log-sql.js'
 
 export default () =>
   new DataLoader(
     async keys => {
+      const trees = [...new Set(keys.map(({ tree }) => tree))]
+
       const ids = keys
         .map(({ ids }) => ids)
         .flat()
         .filter(_ => _)
-      const trees = [...new Set(keys.map(({ tree }) => tree))]
 
       const request = (await pool.connect()).request()
-
       ids.forEach((id, i) => request.input(`id_${i}`, id))
       trees.forEach((tree, i) => request.input(`tree_${i}`, tree))
 
@@ -35,37 +36,37 @@ export default () =>
           join Trees t on t.id = vxt.treeId
         
           where
-            parent.id in (${ids.map((_, i) => `@id_${i}`).join(',')})
-            and t.name in (${trees.map((_, i) => `@tree_${i}`).join(',')})
+            parent.id in (${ids.length ? ids.map((_, i) => `@id_${i}`).join(',') : null})
+            and t.name in (${trees.length ? trees.map((_, i) => `@tree_${i}`).join(',') : null})
         ) p
         left outer join VocabularyXrefVocabulary vxv on vxv.parentId = p.id and vxv.treeId = p.treeId
         left outer join Vocabulary children on children.id = vxv.childId
         
         for json auto;`
 
-      const results = await request.query(sql)
+      try {
+        const results = await request.query(sql)
+        const rows = results.recordset[0]
 
-      /**
-       * Rows are wrapped in 2D array,
-       * take the first item in this wrapper
-       **/
-      const rows = results.recordset[0]
-
-      /**
-       * Return an array for each key
-       * Each key should correspond to an
-       * array of rows:
-       *
-       * [ [row1, row2, row3, row4, etc...] ]
-       *
-       * The DataLoader passes back the inner
-       * array to the resolver:
-       *
-       * [row1, row2, row3, row4, etc...]
-       */
-      return keys.map(({ tree, ids = [] }) => {
-        return rows?.filter(sift({ tree, id: { $in: ids.filter(_ => _) } })) || []
-      })
+        /**
+         * Return an array for each key
+         * Each key should correspond to an
+         * array of rows:
+         *
+         * [ [row1, row2, row3, row4, etc...] ]
+         *
+         * The DataLoader passes back the inner
+         * array to the resolver:
+         *
+         * [row1, row2, row3, row4, etc...]
+         */
+        return keys.map(
+          ({ tree, ids = [] }) =>
+            rows?.filter(sift({ tree, id: { $in: ids.filter(_ => _) } })) || []
+        )
+      } catch (error) {
+        logSql(sql, 'SQL ERROR', true)
+      }
     },
     {
       batch: true,

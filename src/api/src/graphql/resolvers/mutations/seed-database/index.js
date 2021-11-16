219a3478e1579c6d5fd92298668cee744b70d4ac
Zach Smith <zachsmith.sa@gmail.com>
2021-11-16 05:49:49 +0200

    Refactored away more examples of string-concatenation-based SQL (in the seed vocabularies query)

--- src/api/src/graphql/resolvers/mutations/seed-database/index.js
+++ src/api/src/graphql/resolvers/mutations/seed-database/index.js
@@ -4,7 +4,7 @@ import loadVocabularies from './vocabularies/index.js'
 export default async (_, { erm = false, vocabularies = false }, ctx) => {
   const result = {}
 
-  result.vocabularies = vocabularies ? await loadVocabularies(ctx) : false
+  result.vocabularies = vocabularies ? await loadVocabularies() : false
   result.erm = erm ? await loadErmData(ctx) : false
 
   return result
--- src/api/src/graphql/resolvers/mutations/seed-database/vocabularies/index.js
+++ src/api/src/graphql/resolvers/mutations/seed-database/vocabularies/index.js
@@ -3,13 +3,13 @@ import { join } from 'path'
 import getCurrentDirectory from '../../../../../lib/get-current-directory.js'
 import { parse } from 'csv'
 import { performance } from 'perf_hooks'
+import { pool } from '../../../../../mssql/pool.js'
+import mssql from 'mssql'
 
 const __dirname = getCurrentDirectory(import.meta)
 
-export default async ctx => {
+export default async () => {
   const VOCABULARIES = readdirSync(join(__dirname, './trees')).sort()
-
-  const { query } = ctx.mssql
 
   try {
     const t0 = performance.now()
@@ -20,69 +20,87 @@ export default async ctx => {
         parse({ columns: true, skipEmptyLines: true })
       )
 
-      for await (let { parent, term, tree } of parser) {
-        parent = parent.trim()
-        term = term.trim()
-        tree = tree.trim()
+      /**
+       * Each tree is processed into the DB as a single
+       * transaction
+       */
+      const transaction = new mssql.Transaction(await pool.connect())
+      await transaction.begin()
 
-        /**
-         * Make sure that the tree exists in the database
-         */
-        await query(`
-          merge Trees T
-          using (select '${sanitizeSqlValue(tree)}' name) S on T.name = S.name
-          when not matched then insert (name)
-          values (S.name);`)
+      try {
+        for await (let { parent, term, tree } of parser) {
+          parent = parent.trim()
+          term = term.trim()
+          tree = tree.trim()
 
-        /**
-         * Make sure that the terms (parent and child) exists in the database
-         */
-        await query(`
-          merge Vocabulary T
-          using (
-            select distinct *
-            from (
-              select '${sanitizeSqlValue(term)}' term
-              union
-              select '${sanitizeSqlValue(parent)}' term	
-            ) t
-            where term <> '' and term <> ''
-          ) S on T.term = S.term
-          when not matched then insert (term)
-          values (S.term);`)
+          /**
+           * Make sure that the tree exists in the database
+           */
+          await transaction.request().input('tree', tree).query(`
+            merge Trees T
+            using (select @tree name) S on T.name = S.name
+            when not matched then insert (name)
+            values (S.name);`)
 
-        /**
-         * Make sure term is associated with this tree
-         */
-        await query(`
-          merge VocabularyXrefTree T
-          using (
-            select 
-              (select id from Vocabulary where term = '${sanitizeSqlValue(term)}') vocabularyId,
-              (select id from Trees where name = '${sanitizeSqlValue(tree)}') treeId
-          ) S on S.vocabularyId = T.vocabularyId and S.treeId = T.treeId
-          when not matched then insert (vocabularyId, treeId)
-          values (S.vocabularyId, S.treeId);`)
+          /**
+           * Make sure that the terms (parent and child) exists in the database
+           */
+          await transaction.request().input('term', term).input('parent', parent).query(`
+            merge Vocabulary T
+            using (
+              select distinct *
+              from (
+                select @term term
+                union
+                select @parent term	
+              ) t
+              where term <> '' and term <> ''
+            ) S on T.term = S.term
+            when not matched then insert (term)
+            values (S.term);`)
 
-        /**
-         * Insert this tree link
-         */
-        await query(`
-          merge VocabularyXrefVocabulary T
-          using (
-            select * from (
-              select
-                (select id from Vocabulary where term = '${sanitizeSqlValue(parent)}') parentId,
-                (select id from Vocabulary where term = '${sanitizeSqlValue(term)}') childId,
-                (select id from Trees where name = '${sanitizeSqlValue(tree)}') treeId
-            ) t where parentId <> ''
-          ) S on
-          S.parentId = T.parentId
-          and S.childId = T.childId
-          and S.treeId = T.treeId
-          when not matched then insert (parentId, childId, treeId)
-          values (S.parentId, S.childId, S.treeId);`)
+          /**
+           * Make sure term is associated with this tree
+           */
+          await transaction.request().input('term', term).input('tree', tree).query(`
+            merge VocabularyXrefTree T
+            using (
+              select 
+                (select id from Vocabulary where term = @term) vocabularyId,
+                (select id from Trees where name = @tree) treeId
+            ) S on S.vocabularyId = T.vocabularyId and S.treeId = T.treeId
+            when not matched then insert (vocabularyId, treeId)
+            values (S.vocabularyId, S.treeId);`)
+
+          /**
+           * Insert this tree link
+           */
+          await transaction
+            .request()
+            .input('parent', parent)
+            .input('term', term)
+            .input('tree', tree).query(`
+              merge VocabularyXrefVocabulary T
+              using (
+                select * from (
+                  select
+                    (select id from Vocabulary where term = @parent) parentId,
+                    (select id from Vocabulary where term = @term) childId,
+                    (select id from Trees where name = @tree) treeId
+                ) t where parentId <> ''
+              ) S on
+              S.parentId = T.parentId
+              and S.childId = T.childId
+              and S.treeId = T.treeId
+              when not matched then insert (parentId, childId, treeId)
+              values (S.parentId, S.childId, S.treeId);`)
+        }
+      } catch (error) {
+        console.error('Unable to load vocabulary tree', error)
+        await transaction.rollback()
       }
+
+      await transaction.commit()
     }
 
     const t1 = performance.now()

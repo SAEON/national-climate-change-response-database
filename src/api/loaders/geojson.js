8b1aada0862dd2aaa1808aa38582875d2edb3eac
Zach Smith <zachsmith.sa@gmail.com>
2021-11-29 15:16:22 +0200

    Prettier

--- src/api/loaders/geojson.js
+++ src/api/loaders/geojson.js
@@ -1,5 +1,5 @@
-export async function resolve (specifier, ctx, defaultResolve) {
-  const { url } = await defaultResolve(specifier, ctx);
-  const format = url.endsWith('.geojson') ? 'json' : null;
-  return { url, format };
+export async function resolve(specifier, ctx, defaultResolve) {
+  const { url } = await defaultResolve(specifier, ctx)
+  const format = url.endsWith('.geojson') ? 'json' : null
+  return { url, format }
 }

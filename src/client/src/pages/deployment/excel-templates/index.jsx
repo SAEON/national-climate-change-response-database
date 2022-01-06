6800e12572a45191f796a10bec47bbd71cf5caf3
Zach Smith <zachsmith.sa@gmail.com>
2022-01-06 07:26:04 +0200

    Added some documentation on creating tenants to the UI on the tenants page. Helpful note to users

--- src/client/src/pages/deployment/excel-templates/index.jsx
+++ src/client/src/pages/deployment/excel-templates/index.jsx
@@ -2,8 +2,14 @@ import { createPortal } from 'react-dom'
 import Provider from './_context'
 import Uploads from './_uploads'
 import Header from './header'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import useTheme from '@mui/material/styles/useTheme'
+import Typography from '@mui/material/Typography'
 
 export default ({ active, headerRef }) => {
+  const theme = useTheme()
+
   if (!active) {
     return null
   }
@@ -15,6 +21,15 @@ export default ({ active, headerRef }) => {
   return (
     <Provider>
       {createPortal(<Header />, headerRef)}
+      <Card variant="overline">
+        <CardContent>
+          <Typography>
+            Excel templates are shared across all sub-deployments (tenants). Only the most recent
+            template is ever used
+          </Typography>
+        </CardContent>
+      </Card>
+      <div style={{ marginTop: theme.spacing(2) }} />
       <Uploads />
     </Provider>
   )
--- src/client/src/pages/deployment/tenants/index.jsx
+++ src/client/src/pages/deployment/tenants/index.jsx
@@ -2,8 +2,14 @@ import { createPortal } from 'react-dom'
 import Provider from './_context'
 import Table from './table'
 import Header from './header'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import useTheme from '@mui/material/styles/useTheme'
+import Typography from '@mui/material/Typography'
 
 export default ({ active, headerRef }) => {
+  const theme = useTheme()
+
   if (!active) {
     return null
   }
@@ -15,6 +21,17 @@ export default ({ active, headerRef }) => {
   return (
     <Provider>
       {createPortal(<Header />, headerRef)}
+      <Card variant="overline">
+        <CardContent>
+          <Typography>
+            When adding tenants to the deployment, you will need to (1) configure an Nginx server
+            block, (2) update the authentication client with callback URLs suited to the new tenant
+            domain, and (3) restart the application server to configure the new tenant&apos;s
+            authentication client
+          </Typography>
+        </CardContent>
+      </Card>
+      <div style={{ marginTop: theme.spacing(2) }} />
       <Table />
     </Provider>
   )

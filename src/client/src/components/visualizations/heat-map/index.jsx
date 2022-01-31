1ad5081e31b42a483f5f25d54725aff251fe9f7b
Zach Smith <zachsmith.sa@gmail.com>
2022-01-31 10:20:03 +0200

    Testing heatmap fix

--- src/client/src/components/visualizations/heat-map/index.jsx
+++ src/client/src/components/visualizations/heat-map/index.jsx
@@ -12,19 +12,28 @@ window.Heatmap = Heatmap
 export default data => {
   const format = new WKT()
   return new Heatmap({
+    weight: 'normalizedBudget',
     gradient: ['#893448', '#d95850', '#eb8146', '#ffb248', '#f2d643', '#ebdba4'],
     source: new VectorSource({
-      features: data.POINT_LOCATIONS.data.map(({ xy, normalizedBudget }) => {
-        const feature = format.readFeature(xy, {
-          dataProjection: 'EPSG:4326',
-          featureProjection: 'EPSG:4326',
-        })
+      features: data.POINT_LOCATIONS.data
+        .map(({ xy, normalizedBudget }) =>
+          xy
+            .replace('GEOMETRYCOLLECTION (', '')
+            .replace('))', ')')
+            .split(',')
+            .map(point => {
+              const feature = format.readFeature(point.trim(), {
+                dataProjection: 'EPSG:4326',
+                featureProjection: 'EPSG:4326',
+              })
 
-        feature.set('normalizedBudget', normalizedBudget)
-        return feature
-      }),
+              feature.set('normalizedBudget', normalizedBudget)
+              return feature
+            })
+        )
+        .flat(),
     }),
-    blur: 100,
-    radius: 80,
+    blur: 55, // default 15
+    radius: 15, // default 8
   })
 }

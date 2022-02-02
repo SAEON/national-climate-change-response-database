9322e9b3980a72fb4aeafe5d9662d9aadd82befa
Zach Smith <zachsmith.sa@gmail.com>
2022-02-02 06:18:38 +0200

    Fix authorization bug

--- src/api/src/user-model/_ensure-permission.js
+++ src/api/src/user-model/_ensure-permission.js
@@ -19,12 +19,11 @@ export default async (ctx, validTenants, ...permissions) => {
   const { userInfo } = ctx
   const { id: userId } = userInfo
 
-  try {
-    const request = (await pool.connect()).request()
-    request.input('userId', userId)
-    request.input('tenantId', ctx.tenant.id)
-    permissions.forEach(({ name }, i) => request.input(`p_${i}`, name))
-    const result = await request.query(`
+  const request = (await pool.connect()).request()
+  request.input('userId', userId)
+  request.input('tenantId', ctx.tenant.id)
+  permissions.forEach(({ name }, i) => request.input(`p_${i}`, name))
+  const result = await request.query(`
       select 1
       from Permissions p
       join PermissionXrefRole xp on xp.permissionId = p.id
@@ -34,12 +33,9 @@ export default async (ctx, validTenants, ...permissions) => {
         userId = @userId
         and p.name in (${permissions.map((p, i) => `@p_${i}`).join(',')});`)
 
-    const isAuthorized = Boolean(result.recordset.length)
+  const isAuthorized = Boolean(result.recordset.length)
 
-    if (!isAuthorized) {
-      ctx.throw(403)
-    }
-  } catch (error) {
-    console.error('Error (ensurePermission)', error.message)
+  if (!isAuthorized) {
+    ctx.throw(403)
   }
 }
